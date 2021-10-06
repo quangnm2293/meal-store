e0eb9897f87827501783ae4f59cb23cadae4b399
quangnm2293 <quangmn2293@gmail.com>
2021-10-06 23:51:43 +0700

    use redux toolkit & redux-persist & fix issue & fix warning

--- app/store.js
+++ app/store.js
@@ -1,9 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import mealReducer from './mealSlice';
 import modalReducer from './modalSlice';
 import { combineReducers } from 'redux';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 const persistConfig = {
 	key: 'root',
@@ -20,5 +21,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
 	reducer: persistedReducer,
+	middleware: getDefaultMiddleware({
+		serializableCheck: {
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+		},
+	}),
 });
 export const persistor = persistStore(store);
